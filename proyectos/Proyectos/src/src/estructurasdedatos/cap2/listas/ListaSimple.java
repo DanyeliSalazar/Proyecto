5df26f9e682f432632184c37053b6b762c64c905
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-09-17 15:04:20 -0500

    Update ListaSimple.java

--- proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/ListaSimple.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/ListaSimple.java
@@ -5,10 +5,64 @@
  */
 package estructurasdedatos.cap2.listas;
 
-/**
- *
- * @author tusk
- */
+// Leidy Danyeli Salazar 
+
 public class ListaSimple {
+
+    private Nodo cabeza;
+    private Nodo cola;
     
+    public void CrearListaSimple() {
+
+        this.cabeza = null;
+        this.cola = null;
+    }
+
+
+    public void insertar(int dato) {
+        Nodo nod = new Nodo();
+        nod.crearnodo();
+        nod.setDato(dato);
+
+        if (this.cabeza == null && this.cola == null) {
+
+            this.cabeza = nod;
+            this.cola = nod;
+
+        } 
+        else {
+            cola.setSiguiente(nod);
+            this.cola = nod;
+
+        }
+    }
+
+    public void eliminar(int dato) {
+
+        if (cabeza.getDato() == dato) {
+            cabeza =cabeza.getSiguiente();
+        } else {
+            Nodo aux = cabeza;
+            while (aux.getSiguiente().getDato() != dato) {
+                aux = aux.getSiguiente();
+            }
+            Nodo siguiente = aux.getSiguiente().getSiguiente();
+            aux.setSiguiente(siguiente);
+        }
+    }
+
+    public void listar() {
+
+        if (!(this.cabeza == null && this.cola == null)) {
+
+            Nodo aux = cabeza;
+            int i = 0;
+            while (aux != null) {
+                System.out.println("el dato :"  + "[" + aux.getDato() + "]" + " " + "Estas en la Posicion"
+                        + "(" + i + ")" + "de la Lista");
+                aux = aux.getSiguiente();
+                i++;
+            }
+        }
+    }
 }
