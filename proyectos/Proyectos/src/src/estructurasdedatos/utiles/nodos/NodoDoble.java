454b8adfca190296601798397fc48d9d6e0d3b96
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-09-18 21:42:04 -0500

    Update NodoDoble.java

--- proyectos/Proyectos/src/src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -4,21 +4,35 @@
  * and open the template in the editor.
  */
 package lista;
+ 
+public class NodoListaDoble {
+    private int dato;
+    private NodoListaDoble anterior;
+    private NodoListaDoble siguiente;
 
-/**
- *
- * //Leidy Danyeli Salazar
- */
-public class Nododoble<T> {
-    
-   private Nododoble<T>anterior;
-   private Nododoble<T> siguiente;
-   private T dato;
-   
-   public Nododoble (T dato){
-       
-   }
-   
-   
-    
+    public int getDato() {
+        return dato;
+    }
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+    public NodoListaDoble getAnterior() {
+        return anterior;
+    }
+    public void setAnterior(NodoListaDoble anterior){
+        this.anterior = anterior;
+    }
+         public NodoListaDoble getSiguiente() {
+        return siguiente;
+    }
+    public void setSiguiente(NodoListaDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+    public void crearnodoListadoble (){
+        
+        this.siguiente=null;
+        this.anterior=null;
+        this.dato=0;
+        
+    }
 }
