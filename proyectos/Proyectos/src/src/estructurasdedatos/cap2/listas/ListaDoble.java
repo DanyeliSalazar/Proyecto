365a946346fc26c33718c6f1f90479d15c7fd6e2
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-09-18 21:31:40 -0500

    Update ListaDoble.java

--- proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/ListaDoble.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/ListaDoble.java
@@ -3,12 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructurasdedatos.cap2.listas;
+package lista;
 
-/**
- *
- * @author tusk
- */
-public class ListaDoble {
+//Leidy Danyeli Salazar
+public class Listadoble {
+    private NodoListaDoble cabeza;
+    private NodoListaDoble cola;
     
+    public void crearListaDoble() {
+        this.cabeza = null;
+        this.cola = null;
+         }
+    
+     public void insertar(int dato) {
+        NodoListaDoble nod = new NodoListaDoble();
+        nod.crearnodoListadoble();
+        nod.setDato(dato);
+        NodoListaDoble aux = cabeza;
+        if (this.cabeza == null && this.cola == null) {
+            this.cabeza = nod;
+            this.cola = nod;    
+        } else if (nod.getDato() < aux.getDato() && aux.getSiguiente() == null) {
+            nod.setSiguiente(aux);
+            nod.setAnterior(this.cabeza);
+            this.cabeza =nod;    
+        }
+    }
+    
+    public void listar (){
+        if (!(this.cabeza == null && this.cola ==null)){
+            NodoListaDoble aux = cabeza
+        }}
 }
