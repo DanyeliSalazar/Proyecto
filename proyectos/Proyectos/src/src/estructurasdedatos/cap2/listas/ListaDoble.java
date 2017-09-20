f7eac8f3c1831ad74ecdbd2d770720fa06ac3b69
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-09-20 18:00:04 -0500

    Update ListaDoble.java

--- proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/ListaDoble.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/ListaDoble.java
@@ -6,37 +6,74 @@
 package lista;
 
 //Leidy Danyeli Salazar
-public class Listadoble {
+
+public class ListaDoble {
     private NodoListaDoble cabeza;
     private NodoListaDoble cola;
     
     public void crearListaDoble() {
         this.cabeza = null;
         this.cola = null;
-         }
-    
- public void insertar(int dato) {
-        Nodo nod = new Nodo();
-        nod.crearnodo();
-        nod.setDato(dato);
-
+    }
+ 
+    public void insertarOrdenado(int dato) {
+        NodoListaDoble nodito = new NodoListaDoble();
+        nodito.crearnodoListadoble();
+        nodito.setDato(dato);
+        NodoListaDoble aux = cabeza;
         if (this.cabeza == null && this.cola == null) {
-
-            this.cabeza = nod;
-            this.cola = nod;
-
-        } 
-        else {
-            cola.setSiguiente(nod);
-            this.cola = nod;
-
+            this.cabeza = nodito;
+            this.cola = nodito;    
+        } else if (nodito.getDato() < aux.getDato() && aux.getSiguiente() == null) {
+            nodito.setSiguiente(aux);
+            nodito.setAnterior(this.cabeza);
+            this.cabeza =nodito;    
         }
-}
-    
-    public void listar (){
-        if (!(this.cabeza == null && this.cola ==null)){
+    }
+
+    public void listarDoble() {
+        if (!(this.cabeza == null && this.cola == null)) {
             NodoListaDoble aux = cabeza;
-            
+            int i = 0;
+            while (aux != null) {   
+                System.out.println("El Dato :" + " " + "[" + aux.getDato() + "]" + " " + "esta en la posicion:"
+                        + " " + "(" + i + ")" + "de la lista");
+                aux = aux.getSiguiente();  
+                i++;
+            }
+        }
+    }
+
+    public void eliminarDoble(int dato) {
+
+        if (cabeza.getDato() == dato) {
+            cabeza = cabeza.getSiguiente();
+        } else {
+            NodoListaDoble aux = cabeza;
+            while (aux.getSiguiente().getDato() != dato) {
+                aux = aux.getSiguiente();
+            }
+            NodoListaDoble siguiente = aux.getSiguiente().getSiguiente();
+            aux.setSiguiente(siguiente);  
+        }
+    }
+
+    public void insertarFinal(int dato) {
+
+    NodoListaDoble nodito = new NodoListaDoble();
+    nodito.crearnodoListadoble();
+    nodito.setDato(dato);
+    if (this.cabeza == null && this.cola == null) {
+        cabeza = nodito;
+        cola = nodito;
+    } else {
+        NodoListaDoble aux = cabeza;
+        while (aux.getSiguiente() != null) {
+            aux = aux.getSiguiente();
+
+            }
+            aux.setSiguiente(nodito);  
+            nodito.setAnterior(aux);
         }
     }
 }
