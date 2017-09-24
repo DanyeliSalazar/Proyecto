6fbace8f992480781946626af62e9036dffe1c0f
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-09-24 17:10:28 -0500

    Cola

--- proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/Cola.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/Cola.java
@@ -4,11 +4,77 @@
  * and open the template in the editor.
  */
 package estructurasdedatos.cap2.listas;
+import estructurasdedatos.cap2.listas.Persona;
 
-/**
- *
- * @author tusk
- */
+//Leidy Danyeli Salazar
+
 public class Cola {
-    
+
+    private Nodopersona dato;
+    private Nodopersona limite;
+    private Nodopersona ultimo;
+
+    public void creacola() {
+        limite = ultimo = null;
+    }
+
+    public boolean vacia() {
+        return (this.limite == null);
+    }
+
+    public void encolar(int DocumentoDeIdentidad) {
+
+        Nodopersona nuevo = new Nodopersona();
+        nuevo.setDato(DocumentoDeIdentidad);
+        if (vacia()) {
+            this.limite = nuevo;
+            this.ultimo = nuevo;
+        } else {
+            ultimo.setSiguiente(nuevo);
+            ultimo = nuevo;
+        }
+    }
+
+    public void desencolar() {
+
+        if (vacia()) {
+            System.out.println("La Cola esta vacia");
+        } else {
+            limite = limite.getSiguiente();
+        }
+    }
+
+    public void listar() {
+        Nodopersona aux = limite;
+
+        System.out.println("");
+        int i = 0;
+       
+        
+        while (aux != null) {
+            System.out.println(i + "." + " [ " + aux.getDato().getDocumentoDeIdentidad() + " ]");
+            aux = aux.getSiguiente();
+            i++;
+        }
+    }
+
+    public int tamaño() {
+
+        Nodopersona aux = limite;
+        int contador = 0;
+        while (aux != null) {
+            contador++;
+            aux = aux.getSiguiente();
+        }
+        return contador;
+    }
+
+    public Nodopersona getDato() {
+        return dato;
+    }
+
+    public void setDato(int Documento De Identidad) {
+        Nodopersona p = new Nodopersona();
+        p = dato;
+    }
 }
