0afb24a4eead7d429a8bd3d3cc1c3af58f052f48
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-09-24 17:11:07 -0500

    Pila

--- proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/Pila.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap2/listas/Pila.java
@@ -3,12 +3,66 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructurasdedatos.cap2.listas;
 
-/**
- *
- * @author tusk
- */
+package estructurasdedatos.cap2.listas;
+import estructurasdedatos.cap2.listas.Tallerpila;
+
+//Leidy Danyeli Salazar
+
 public class Pila {
+    private Datospila dato;
+    private Tallerpila tope;
     
+    
+    public Datospila getDato() {
+        return dato;
 }
+
+    
+    public void setDato(Datospila dato) {
+        this.dato = dato;
+}
+
+    public Pila(){
+        dato = null;  
+}
+
+    public boolean vacia(){
+        return (tope == null);
+}
+    
+       public void apilar(String nombre, String fecha, String responsable){
+        Tallerpila nuevo =new Tallerpila();
+        nuevo.setDato(nombre, fecha, responsable);
+        if(vacia()){
+            tope=nuevo;
+
+        }
+else{
+        nuevo.setSiguiente(tope);
+        tope = nuevo;
+        }
+    }
+      
+       public void desapilar() {
+        if (vacia()) {
+            System.out.println("No existen Esos Elementos En La Pila");
+        } else {
+            System.out.println("dato ( " + tope.getDato().getNombre() + " )" + " Eliminado");
+            tope = tope.getSiguiente();
+        }
+    }
+   
+        public void listar() {
+
+        if (!(tope == null)) {
+            Tallerpila aux = tope;
+            System.out.println("La Pila Es ");
+            while (aux != null) {
+                System.out.println("el dato :" + "[" + aux.getDato().getNombre() + "]");
+                aux = aux.getSiguiente();
+            }
+        }
+    }
+}
+
