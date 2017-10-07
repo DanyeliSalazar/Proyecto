6049f233832d8aee1f39995f92c9b032db9a8cd4
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-10-06 19:14:43 -0500

    Update Diccionario.java

--- proyectos/Proyectos/src/src/estructurasdedatos/cap3/mapas/Diccionario.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap3/mapas/Diccionario.java
@@ -1,14 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructurasdedatos.cap3.mapas;
 
-/**
- *
- * @author tusk
- */
+
+import estructurasdedatos.utiles.nodos.NodoPdic;
+
+/*@author Leidy Danyeli Salazar Murillo*/
 public class Diccionario {
+
+    NodoPdic cabeza;
+    NodoPdic cola;
+
+    public Diccionario() {
+        cabeza = null;
+        cola = null;
+    }
+
     
+    public void ingresarPdic(String Pdic, String significa) {
+        NodoPdic nuevaPdic = new NodoPdic();
+        nuevaPdic.setPdic(Pdic);
+        nuevaPdic.setsignifica(significa);
+
+        
+        if (cabeza == null) {
+            cabeza = nuevaPdic;
+            cabeza.setsignifica(null);
+            cola = cabeza;
+
+            
+        } else {
+            cola.setsiguiente(nuevaPdic);
+            nuevaPdic.setsignifica(null);
+            cola = nuevaPdic;
+        }
+        
+    }
+
+    
+    
+    public void verPdic() {
+        NodoPdic palabra = new NodoPdic();
+        palabra = cabeza;
+        while (palabra!= null) {
+            System.out.println(palabra.getPdic() + " " + palabra.getsignifica());
+            palabra = palabra.getsiguiente();
+        }
+        System.out.println("");
+        
+    }
+
+    
+    public void modificarpaldicc(String pal, String nuevosignifica) {
+        NodoPdic palabra  = new NodoPdic();
+       palabra  = cabeza;
+        int estado = 0;
+        while (palabra  != null) {
+            if (palabra .getPdic().equals(pal) == true) {
+
+                palabra .setsignifica(nuevosignifica);
+                estado = 1;
+
+            }
+            palabra  = palabra .getsiguiente();
+        }
+        if (estado == 0) {
+            System.out.println("No esta en el diccionario");
+        }
+    }
+
+    
+    
+    public void borrarPdic(String pal) {
+        NodoPdic palabra = new NodoPdic();
+        NodoPdic anterior = new NodoPdic();
+         palabra = cabeza;
+        anterior = null;
+        int ban = 0;
+        while (palabra!= null) {
+            if (palabra.getPdic().equals(pal) == true) {
+                ban = 1;
+                if (palabra == cabeza) {
+                    cabeza = cabeza.getsiguiente();
+                } else {
+                    anterior.setsiguiente(palabra.getsiguiente());
+          
+                
+                }
+
+            }
+            anterior = palabra;
+           palabra = palabra.getsiguiente();
+        }
+        if (ban == 0) {
+            System.out.println("No esta en el diccionario");
+        }
+    }
+
 }
