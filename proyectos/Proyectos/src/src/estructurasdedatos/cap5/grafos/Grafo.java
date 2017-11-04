cb839183af49146477669410b4f23e0d80aead4f
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-11-03 21:28:32 -0500

    Update Grafo.java

--- proyectos/Proyectos/src/src/estructurasdedatos/cap5/grafos/Grafo.java
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap5/grafos/Grafo.java
@@ -5,10 +5,35 @@
  */
 package estructurasdedatos.cap5.grafos;
 
+import java.util.List;
+
 /**
- *
- * @author tusk
- */
-public class Grafo {
-    
-}
+* 
+* @author Leidy Danyeli Salazar M 
+*/
+        
+        public class Grafo {
+            
+                List<Vertice> vertices;    
+                List<ArcoNoDirigido> matrizadyacensia;
+            
+        public void insertarVertice(String dato) {
+                Vertice vertice = new Vertice();
+                    vertice.setDato(dato);    
+            }
+       
+            
+            public void insertarArco(Vertice v1, Vertice v2) {
+                    ArcoNoDirigido Nuevoarco = new ArcoNoDirigido();       
+                    Nuevoarco.setV1(v1);        
+                    Nuevoarco.setV2(v2);        
+                    matrizadyacensia.add(Nuevoarco);
+            }        
+            
+            public void eliminarvertice(String dato){        
+                for (Vertice verticeactual : vertices) {
+                    if (verticeactual.getDato().compareTo(dato)==0)                
+                        vertices.remove(verticeactual);                               
+                }    
+            }
+        }
