62ea550fd5359720f89b005c01b70cea8255bdc7
DanyeliSalazar <30839375+DanyeliSalazar@users.noreply.github.com>
2017-11-03 20:49:14 -0500

    Create Vertice.java

---
+++ proyectos/Proyectos/src/src/estructurasdedatos/cap5/grafos/Vertice.java
@@ -0,0 +1,22 @@
+/* 
+* To change this license header, choose License Headers in Project Properties. 
+* To change this template file, choose Tools | Templates 
+* and open the template in the editor. 
+*/
+
+package estructurasdedatos.cap5.grafos;
+/**
+* 
+* @author Leidy Danyeli Salazar 
+*/
+
+  public class Vertice {    
+     private String dato; 
+       
+       public String getDato() {
+              return dato;
+}
+      public void setDato(String dato) {
+              this.dato = dato;    
+     } 
+}
